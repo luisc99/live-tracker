c0d133d3381c27137b6b3d45b0ca49beab71af92
cylorun <allidabbi@gmail.com>
2024-04-17 15:00:42 +0000

    more log stuff

--- src/main/java/me/cylorun/event/LogHandler.java
+++ src/main/java/me/cylorun/event/LogHandler.java
@@ -4,9 +4,11 @@ import me.cylorun.enums.LogEvent;
 import me.cylorun.event.callbacks.LogEventListener;
 import me.cylorun.io.minecraft.WorldFile;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogHandler extends Thread {
@@ -28,20 +30,45 @@ public class LogHandler extends Thread {
         }
     }
 
+    private List<String> getChanges(List<String> lines) {
+        List<String> newLines = new ArrayList<>();
+        int endIdx = 0;
+        for (int i = lines.size() - 1; i > 0; i--) {
+            if (lines.get(i).equals(this.lastPreviousLine)) {
+                endIdx = i;
+                break;
+            }
+        }
+        for (int i = endIdx; i < lines.size(); i++) {
+            newLines.add(lines.get(i));
+        }
+        return newLines;
+    }
+
     @Override
     public void run() {
         while (true) {
             try {
                 File logFile = this.file.getLogPath().toFile();
-                if (logFile.length() > this.lastSize) {
+                if (logFile.length() > this.lastSize) { // log has updated
                     this.lastSize = logFile.length();
-                    FileReader reader = new FileReader(logFile);
+                    BufferedReader reader = new BufferedReader(new FileReader(logFile));
+                    List<String> lines = new ArrayList<>();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        lines.add(line);
+                    }
 
+                    List<String> newLines = this.getChanges(lines);
+
+                    for (String l : newLines){
+
+                    }
                 }
 
 
                 Thread.sleep(this.DELAY);
-            } catch (InterruptedException | FileNotFoundException e) {
+            } catch (InterruptedException | IOException e) {
                 throw new RuntimeException(e);
             }
         }
