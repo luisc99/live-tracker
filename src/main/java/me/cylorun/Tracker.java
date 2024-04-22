8d9aba3e1f6c33f7374bacf4fe1457e4a0f9e594
cylorun <allidabbi@gmail.com>
2024-04-22 14:46:20 +0000

    ui shit

--- src/main/java/me/cylorun/Tracker.java
+++ src/main/java/me/cylorun/Tracker.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 
 public class Tracker {
+
+    public static final String VERSION = "0.0.0";
     public static void main(String[] args) {
         GoogleSheetsClient.setup();
         Logging.info("Tracking");
--- src/main/java/me/cylorun/gui/TrackerFrame.java
+++ src/main/java/me/cylorun/gui/TrackerFrame.java
@@ -1,4 +1,30 @@
 package me.cylorun.gui;
 
-public class TrackerFrame {
+import com.formdev.flatlaf.FlatDarculaLaf;
+import me.cylorun.Tracker;
+import org.lwjgl.system.CallbackI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TrackerFrame extends JFrame {
+
+    private final String NAME = "Live-Tracker"+ Tracker.VERSION;
+    private Point windowDragStartCoords;
+    public TrackerFrame() throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new FlatDarculaLaf());
+        this.setSize(400, 300);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton settingsButton = new JButton("Settings");
+
+        settingsButton.addActionListener((e)->{
+
+        });
+        this.add(settingsButton);
+    }
+
+
 }
