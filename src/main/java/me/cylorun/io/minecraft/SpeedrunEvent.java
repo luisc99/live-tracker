2d76cbbe0d418accdf66ce5b6398ea80c4b51bc0
cylorun <allidabbi@gmail.com>
2024-04-29 18:54:43 +0000

    fixed a rare crash

--- src/main/java/me/cylorun/io/minecraft/SpeedrunEvent.java
+++ src/main/java/me/cylorun/io/minecraft/SpeedrunEvent.java
@@ -1,18 +1,21 @@
 package me.cylorun.io.minecraft;
 
 import me.cylorun.enums.SpeedrunEventType;
+import me.cylorun.utils.Assert;
 
 public class SpeedrunEvent {
     public SpeedrunEventType type;
-    public Integer igt;
-    public Integer rta;
+    public Long igt;
+    public Long rta;
 
     public SpeedrunEvent(String logString) { // common.multiplayer rta igt
         String[] split = logString.split(" ");
+        Assert.isTrue(split.length == 3, "invalid speedrunevent string");
         this.type = stringToSREvent(split[0].split("\\.")[1]);
-        this.rta = Integer.parseInt(split[1]);
-        this.igt = Integer.parseInt(split[2]);
+        this.rta = Long.parseLong(split[1]);
+        this.igt = Long.parseLong(split[2]);
     }
+
     public static SpeedrunEventType stringToSREvent(String s) {
         return SpeedrunEventType.valueOf(s.toUpperCase());
     }
